fd6c5e7eabbd2e5f5db0dad0b0663d001f71cac8
Haipeng Guo <haipeng.guo@signanthealth.com>
2022-08-02 18:21:31 +0300

    add GetEquipment lambda

--- backend/contract_tracker/handlers/get-equipment/app.js
+++ backend/contract_tracker/handlers/get-equipment/app.js
@@ -4,11 +4,18 @@ let response;
 
 exports.lambdaHandler = async (event, context) => {
     try {
-        // console.log("event.body, %s", event.body);
+        console.log("event.body, %s", event.body);
         // const ret = await axios(url);
+        const mockResp = {
+            equipmentNumber: "en_12345",
+            address: "mock_address",
+            startDate: "mock_start_date",
+            endDate: "mock_end_date",
+            status: "running",
+        }
         response = {
             'statusCode': 200,
-            'body': JSON.stringify(event.body)
+            'body': JSON.stringify(mockResp)
         }
     } catch (err) {
         console.log(err);
