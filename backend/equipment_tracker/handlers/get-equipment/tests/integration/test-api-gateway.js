75452a0d805e73daab7645d5f2d5863b846a4f3e
Haipeng Guo <haipeng.guo@signanthealth.com>
2022-08-05 01:43:35 +0300

    add integration test for (GET) /equipment/{equipmentNumber}

--- backend/equipment_tracker/handlers/get-equipment/tests/integration/test-api-gateway.js
+++ backend/equipment_tracker/handlers/get-equipment/tests/integration/test-api-gateway.js
@@ -46,25 +46,42 @@ describe("Test (GET) /equipment/{equipmentNumber}", function () {
   before(async () => {
     apiEndpoint = await testingUtils.getApiEndpoint()
     console.log("apiEndpoint: %s", apiEndpoint)
-    // await createEquipments(apiEndpoint)
   })
 
-  // it("returns 201, OK", (done) => {
-  //   console.info("api endpoint:", apiEndpoint);
-  //   const path = apiEndpoint + 'equipment/it_get_eq_1'
-  //
-  //   // await axios.get(path)
-  //
-  //   axios.get(path)
-  //       .then(res => {
-  //       console.log(`statusCode: ${res.status}`);
-  //       expect(res.status).to.equal(200);
-  //       expect(res.data.EquipmentNumber).to.equal("it_get_eq_1");
-  //       done()
-  //     })
-  //     .catch(error => {
-  //       console.error(error);
-  //       throw error
-  //     });
-  // });
-});
+  it("Successful, returns 200, OK", async () => {
+    try{
+      const path = apiEndpoint + 'equipment'
+      const payload = {
+        EquipmentNumber: "en_12345",
+        Address: "address_1",
+        StartDate: "start_date_1",
+        EndDate: "end_date_1",
+        Status: "Running",
+      }
+      const res = await axios.post(path, payload)
+      expect(res.status).to.equal(201)
+
+      const pathGet = apiEndpoint + 'equipment/en_12345'
+      const resGet = await axios.get(pathGet)
+      expect(resGet.status).to.equal(200)
+      expect(res.data.EquipmentNumber).to.equal("en_12345")
+    }catch (err){
+      throw err
+    } finally {
+      const pathDelete = apiEndpoint + 'equipment/en_12345'
+      const resDelete = await axios.delete(pathDelete)
+      expect(resDelete.status).to.equal(200)
+    }
+  })
+
+  it("Failed, returns 404, not found", async () => {
+    try{
+      const pathGet = apiEndpoint + 'equipment/en_12345'
+      const resGet = await axios.get(pathGet)
+      expect(resGet).to.be.null
+    }catch (err){
+      expect(err.response.status).to.equal(404)
+    }
+  })
+
+})
